4acb5700323612d52ce318b2a7c155ec3ab0f1ad
0xmugen <pierre.semanne@gmail.com>
2024-03-26 17:43:46 +0100

    fix: remove flag overrides, and have teh flag accessible globaly

--- config/config.go
+++ config/config.go
@@ -6,6 +6,10 @@ import (
 	"os"
 )
 
+var (
+	configPath = flag.String("config", "config.json", "The path to the configuration file")
+)
+
 type ExecutionConfig struct {
 }
 
@@ -25,17 +29,12 @@ type AppConfig struct {
 	Storage   StorageConfig   `json:"storageConfig"`
 }
 
-type ConfigOverrides struct {
-	Port     int
-	MaxPeers int
-}
-
 func LoadConfig() (*AppConfig, error) {
-	configPath, overrides := parseFlags()
+	flag.Parse()
 
 	cfg := &AppConfig{}
 
-	file, err := os.Open(configPath)
+	file, err := os.Open(*configPath)
 	if err != nil {
 		return cfg, err
 	}
@@ -45,33 +44,5 @@ func LoadConfig() (*AppConfig, error) {
 		return cfg, err
 	}
 
-	applyOverrides(cfg, overrides)
-
 	return cfg, nil
-}
-
-func parseFlags() (string, ConfigOverrides) {
-	var configPath string
-	var overrides ConfigOverrides
-
-	flag.StringVar(&configPath, "config", "./config.json", "Path to configuration file")
-	flag.StringVar(&configPath, "c", "./config.json", "Path to configuration file (shorthand)")
-
-	flag.IntVar(&overrides.Port, "port", 0, "Port to override the one in the configuration file")
-	flag.IntVar(&overrides.Port, "p", 0, "Port to override the one in the configuration file")
-
-	flag.IntVar(&overrides.MaxPeers, "peers", 0, "MaxPeers to override the one in the configuration file")
-
-	flag.Parse()
-
-	return configPath, overrides
-}
-
-func applyOverrides(cfg *AppConfig, overrides ConfigOverrides) {
-	if overrides.Port > 0 {
-		cfg.Network.Port = overrides.Port
-	}
-	if overrides.MaxPeers > 0 {
-		cfg.Network.MaxPeers = overrides.MaxPeers
-	}
 }
