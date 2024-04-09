e19c8f6df7c7c66441ed5c322370a8546c5a55fc
0xmugen <pierre.semanne@gmail.com>
2024-04-09 15:57:27 +0200

    feat: add more logging

--- cmd/bootstrap/main.go
+++ cmd/bootstrap/main.go
@@ -17,16 +17,21 @@ func main() {
 		port = defaultPort
 	}
 	listenAddr := fmt.Sprintf("/ip4/0.0.0.0/tcp/%s", port)
+	fmt.Printf("Configured to listen on %s\n", listenAddr)
 
 	bootstrapNode, err := bootstrap.New(ctx, listenAddr)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Failed to create bootstrap node: %v\n", err)
 		os.Exit(1)
+	} else {
+		fmt.Println("Bootstrap node created successfully.")
 	}
 
 	if err := bootstrapNode.Start(); err != nil {
 		fmt.Fprintf(os.Stderr, "Failed to start bootstrap node: %v\n", err)
 		os.Exit(1)
+	} else {
+		fmt.Println("Bootstrap node started successfully.")
 	}
 
 	select {}
