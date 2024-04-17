ee4a66c8ae3781207126cfc618a85b8d62b7bd2a
Jihoon Song <jihoonsong@users.noreply.github.com>
2024-04-17 22:53:14 +0900

    Add ramd

---
+++ ramd/src/main.rs
@@ -0,0 +1,3 @@
+fn main() {
+    println!("Topology is a community-driven technology that brings random access memory to the world computer to power lock-free asynchronous decentralized applications.");
+}
