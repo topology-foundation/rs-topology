cc5080256c6a24ae811d7b734b5462487d828c2d
Jihoon Song <jihoonsong@users.noreply.github.com>
2024-04-24 22:51:57 +0900

    Keep JSON-RPC handler simple

--- crates/rpc/jsonrpc/src/live_object.rs
+++ crates/rpc/jsonrpc/src/live_object.rs
@@ -1,9 +1,7 @@
 use std::sync::Arc;
 
 use async_trait::async_trait;
-use base64::prelude::{Engine, BASE64_STANDARD};
 use jsonrpsee::core::RpcResult;
-use jsonrpsee::types::{error::ErrorObject, ErrorCode};
 use ramd_jsonrpc_api::server::LiveObjectApiServer;
 use ramd_jsonrpc_types::live_object::CreateLiveObject;
 use ramd_node::LiveObjectHandler;
@@ -33,16 +31,7 @@ where
     async fn create_live_object(&self, request: CreateLiveObject) -> RpcResult<()> {
         info!(target: "ramd::jsonrpc", "Request to create a live object with wasm bytes {}", request.wasm_bytes);
 
-        let wasm_bytes = match BASE64_STANDARD.decode(request.wasm_bytes) {
-            Ok(bytes) => bytes,
-            Err(e) => {
-                info!(target: "ramd::jsonrpc", "Failed to decode wasm bytes with error `{}`", e.to_string());
-
-                return Err(ErrorObject::from(ErrorCode::InvalidParams));
-            }
-        };
-
-        self.node.create_live_object(wasm_bytes);
+        self.node.create_live_object(request.decode_wasm_bytes()?);
 
         Ok(())
     }
